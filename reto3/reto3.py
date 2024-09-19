b8bd9c2768778f31781215504acba8dc081cbab5
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-09-19 00:15:12 -0500

    volumen v1

--- reto3/reto3.py
+++ reto3/reto3.py
@@ -3,3 +3,8 @@ Julio Rocha
 Liceth Rodriguez
 Marcy Castro
 """
+import math
+radio = float(input("ingresa el radio de la pelota o de la corcunferencia: "))
+Vpelota = (4/3)*math.pi*(radio**3)
+VRpelota = round (Vpelota,3)
+print ("el volumen de la pelota es: ", VRpelota, "metros cubico")
