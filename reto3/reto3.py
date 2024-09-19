affda4932817ce0786df0684baa33953464dd6ad
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-09-19 00:25:50 -0500

    volumen v1.1

--- reto3/reto3.py
+++ reto3/reto3.py
@@ -3,14 +3,14 @@ Julio Rocha
 Liceth Rodriguez
 Marcy Castro
 """
-
+#Imoortamos la libreria math
 import math
 #se le da la bienvenida al usuario
 print ("ienvenido a la calculadora que te permitira conocer el volumen de la pelota segun el radio")
 #Se le solicicta al usuario que ingrese el radio de la circunferencia o pelota
 radio = float(input("ingresa el radio de la pelota o de la corcunferencia: "))
 #Se utiliza la formula para conocer el volumen de la esfera concodiendo su radio
-Vpelota = (4/3)*math.pi*(radio**3)
+Vpelota = (4/3)*math.pi*(radio**3) #la funcion math.pi() funciona como el numero pi
 #se utliza la funcion round() para solo utilizar 
 VRpelota = round (Vpelota,3)
 #se le muestra al ususario el resultado de la operacion
