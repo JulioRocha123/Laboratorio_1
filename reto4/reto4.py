96a7505881c3eaefa5e9244222d838ad45cb3471
Julio Edilberto Rocha Trujillo <rochajuliowork@gmail.com>
2024-09-20 11:20:17 -0500

    reto 4 V1

--- reto4/reto4.py
+++ reto4/reto4.py
@@ -3,3 +3,21 @@ Julio Rocha
 Liceth Rodriguez
 Marcy Castro
 """
+#Se le solicta al usuario que ingrese los datos
+nivel = int(input("Ingresa tu nivel de acceso: "))
+cambio = input("Su contraseña fue cambiada en un plazo de 30 dias? Si | No ")
+tarjeta = input("Su tarjeta se encuentra activa? Si | No ")
+#se recisa que el nicel de acceso sea necesario
+if nivel >= 1 and nivel <= 5:
+        #Se verifica que el usuario tenga la tarjeta activa
+        if tarjeta == "Si":
+            # se verifica que el ususario haya cambiado la contraseña en periodo de 30 dias
+            if cambio == "Si":
+                #se le da acceso al ususario con todas las verificaciones 
+                print ("Acceso permitido")
+            else:
+                print ("Acceso denegado: Debe cambiar la contraseña.")
+        else:
+            print ("Acceso denegado: Tarjeta inactiva.")
+else:
+        print ("Acceso denegado: Nivel de acceso insuficiente.")
